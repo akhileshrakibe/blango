85a32c65195e2ce2182cbf76eaad1f4c7c49e696
Akhilesh Rakibe <akhileshrakibe@gmail.com>
2024-06-13 10:06:26 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -51,6 +51,7 @@ class Dev(Configuration):
         'allauth.account',
         'allauth.socialaccount',
         'allauth.socialaccount.providers.google',
+        'rest_framework',
         
     ]
 
