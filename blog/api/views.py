fca6104f02e02fe9aa8c9ed8d6a785a7505598f0
Akhilesh Rakibe <akhileshrakibe@gmail.com>
2024-06-17 10:00:30 +0000

    Finish throttling

--- blog/api/views.py
+++ blog/api/views.py
@@ -39,32 +39,32 @@ class PostViewSet(viewsets.ModelViewSet):
             return PostSerializer
         return PostDetailSerializer
 
-    @method_decorator(cache_page(300))
-    @method_decorator(vary_on_headers("Authorization"))
-    @method_decorator(vary_on_cookie)
-    @action(methods=["get"], detail=False, name="Posts by the logged in user")
-    def mine(self, request):
-      if request.user.is_anonymous:
-          raise PermissionDenied("You must be logged in to see which Posts are yours")
-      posts = self.get_queryset().filter(author=request.user)
-      serializer = PostSerializer(posts, many=True, context={"request": request})
-      return Response(serializer.data)
+    # @method_decorator(cache_page(300))
+    # @method_decorator(vary_on_headers("Authorization"))
+    # @method_decorator(vary_on_cookie)
+    # @action(methods=["get"], detail=False, name="Posts by the logged in user")
+    # def mine(self, request):
+    #   if request.user.is_anonymous:
+    #       raise PermissionDenied("You must be logged in to see which Posts are yours")
+    #   posts = self.get_queryset().filter(author=request.user)
+    #   serializer = PostSerializer(posts, many=True, context={"request": request})
+    #   return Response(serializer.data)
 
-    @method_decorator(cache_page(120))
-    def list(self, *args, **kwargs):
-        return super(PostViewSet, self).list(*args, **kwargs)
+    # @method_decorator(cache_page(120))
+    # def list(self, *args, **kwargs):
+    #     return super(PostViewSet, self).list(*args, **kwargs)
 
-    @method_decorator(cache_page(300))
-    def get(self, *args, **kwargs):
-        return super(UserDetail, self).get(*args, *kwargs)
+    # @method_decorator(cache_page(300))
+    # def get(self, *args, **kwargs):
+    #     return super(UserDetail, self).get(*args, *kwargs)
 
-    @method_decorator(cache_page(300))
-    def list(self, *args, **kwargs):
-        return super(TagViewSet, self).list(*args, **kwargs)
+    # @method_decorator(cache_page(300))
+    # def list(self, *args, **kwargs):
+    #     return super(TagViewSet, self).list(*args, **kwargs)
 
-    @method_decorator(cache_page(300))
-    def retrieve(self, *args, **kwargs):
-        return super(TagViewSet, self).retrieve(*args, **kwargs)
+    # @method_decorator(cache_page(300))
+    # def retrieve(self, *args, **kwargs):
+    #     return super(TagViewSet, self).retrieve(*args, **kwargs)
 
 class UserDetail(generics.RetrieveAPIView):
     lookup_field = "email"
