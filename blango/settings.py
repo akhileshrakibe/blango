8d42b0dd8b1325e6b7128bb93e9ad7f477302b41
Akhilesh Rakibe <akhileshrakibe@gmail.com>
2024-06-18 06:16:56 +0000

    Finish pagination and django filter

--- blango/settings.py
+++ blango/settings.py
@@ -54,7 +54,7 @@ class Dev(Configuration):
         'allauth.socialaccount.providers.google',
         'rest_framework',
         'rest_framework.authtoken',
-        
+        'django_filters',
         
     ]
 
@@ -238,6 +238,18 @@ class Dev(Configuration):
             "user_sustained": "5000/day",
             "user_burst": "100/minute",
         },
+
+        "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+        "PAGE_SIZE": 100,
+
+        "DEFAULT_FILTER_BACKENDS": [
+            "django_filters.rest_framework.DjangoFilterBackend"
+        ],
+
+        "DEFAULT_FILTER_BACKENDS": [
+            "django_filters.rest_framework.DjangoFilterBackend",
+            "rest_framework.filters.OrderingFilter"
+        ],
         
     }
 
--- blog/api/views.py
+++ blog/api/views.py
@@ -26,52 +26,19 @@ from django.utils import timezone
 from datetime import timedelta
 from django.http import Http404
 
-# class PostList(generics.ListCreateAPIView):
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
-
-
-# class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Post.objects.all()
-#     serializer_class = PostDetailSerializer
-#     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
+from blog.api.filters import PostFilterSet
 
 class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
-
+    filterset_fields = ["author", "tags"]
+    filterset_class = PostFilterSet
+    ordering_fields = ["published_at", "author", "title", "slug"]
+    
     def get_serializer_class(self):
         if self.action in ("list", "create"):
             return PostSerializer
         return PostDetailSerializer
-
-    @method_decorator(cache_page(300))
-    @method_decorator(vary_on_headers("Authorization"))
-    @method_decorator(vary_on_cookie)
-    @action(methods=["get"], detail=False, name="Posts by the logged in user")
-    def mine(self, request):
-      if request.user.is_anonymous:
-          raise PermissionDenied("You must be logged in to see which Posts are yours")
-      posts = self.get_queryset().filter(author=request.user)
-      serializer = PostSerializer(posts, many=True, context={"request": request})
-      return Response(serializer.data)
-
-    @method_decorator(cache_page(120))
-    @method_decorator(vary_on_headers("Authorization", "Cookie"))
-    def list(self, *args, **kwargs):
-        return super(PostViewSet, self).list(*args, **kwargs)
-
-    @method_decorator(cache_page(300))
-    def get(self, *args, **kwargs):
-        return super(UserDetail, self).get(*args, *kwargs)
-
-    @method_decorator(cache_page(300))
-    def list(self, *args, **kwargs):
-        return super(TagViewSet, self).list(*args, **kwargs)
-
-    @method_decorator(cache_page(300))
-    def retrieve(self, *args, **kwargs):
-        return super(TagViewSet, self).retrieve(*args, **kwargs)
 
     def get_queryset(self):
       if self.request.user.is_anonymous:
@@ -107,14 +74,42 @@ class PostViewSet(viewsets.ModelViewSet):
           raise Http404(
               f"Time period {time_period_name} is not valid, should be "
               f"'new', 'today' or 'week'"
-          )
+          )    
+
+    @method_decorator(cache_page(300))
+    @method_decorator(vary_on_headers("Authorization"))
+    @method_decorator(vary_on_cookie)
+    @action(methods=["get"], detail=False, name="Posts by the logged in user")
+    def mine(self, request):
+        if request.user.is_anonymous:
+            raise PermissionDenied("You must be logged in to see which Posts are yours")
+        posts = self.get_queryset().filter(author=request.user)
+
+        page = self.paginate_queryset(posts)
+
+        if page is not None:
+            serializer = PostSerializer(page, many=True, context={"request": request})
+            return self.get_paginated_response(serializer.data)
+
+        serializer = PostSerializer(posts, many=True, context={"request": request})
+        return Response(serializer.data)
+
+    @method_decorator(cache_page(120))
+    @method_decorator(vary_on_headers("Authorization", "Cookie"))
+    def list(self, *args, **kwargs):
+        return super(PostViewSet, self).list(*args, **kwargs)
+
 
 class UserDetail(generics.RetrieveAPIView):
     lookup_field = "email"
     queryset = User.objects.all()
     serializer_class = UserSerializer    
 
+    @method_decorator(cache_page(300))
+    def get(self, *args, **kwargs):
+        return super(UserDetail, self).get(*args, *kwargs)
 
+    
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
@@ -122,10 +117,22 @@ class TagViewSet(viewsets.ModelViewSet):
     @action(methods=["get"], detail=True, name="Posts with the Tag")
     def posts(self, request, pk=None):
         tag = self.get_object()
+        page = self.paginate_queryset(tag.posts)
+        if page is not None:
+            post_serializer = PostSerializer(
+                page, many=True, context={"request": request}
+            )
+            return self.get_paginated_response(post_serializer.data)
         post_serializer = PostSerializer(
             tag.posts, many=True, context={"request": request}
         )
         return Response(post_serializer.data)
     
+    @method_decorator(cache_page(300))
+    def list(self, *args, **kwargs):
+        return super(TagViewSet, self).list(*args, **kwargs)
 
+    @method_decorator(cache_page(300))
+    def retrieve(self, *args, **kwargs):
+        return super(TagViewSet, self).retrieve(*args, **kwargs)
 
